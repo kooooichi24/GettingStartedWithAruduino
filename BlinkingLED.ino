c86dccaf71237732ea6581d7d45facf9f8e48fcf
古川広一 <ex.kouichi1224@gmail.com>
2019-10-31 00:25:29 +0900

    install and set Arduino for Visual Studio Code. We can skech ArduinoFile.ino on vscode.

---
+++ BlinkingLED.ino
@@ -0,0 +1,16 @@
+// Example 01 : Blinking LED
+
+const int LED = 13;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(LED, HIGH);
+  delay(5000);
+  digitalWrite(LED, LOW);
+  delay(5000);
+}
